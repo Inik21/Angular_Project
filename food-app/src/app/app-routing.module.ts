9f367c9c55e2a4104eaf1d4ce0e5d793c72adc85
Inik_21 <n.dzhunov@yahoo.com>
2020-12-15 19:32:30 +0200

    Add userModule Add loginComponent Add login route

--- food-app/src/app/app-routing.module.ts
+++ food-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './user/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'user/login',
+    component: LoginComponent
+  }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const AppRoutingModule = RouterModule.forRoot(routes);
