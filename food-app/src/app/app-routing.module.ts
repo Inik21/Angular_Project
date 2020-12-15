d60a570eac84aefbdcd907bc16ae98d5272018c8
Inik_21 <n.dzhunov@yahoo.com>
2020-12-15 20:45:42 +0200

    Add registerComponent Add register route

--- food-app/src/app/app-routing.module.ts
+++ food-app/src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
 
 const routes: Routes = [
   {
     path:'user/login',
     component: LoginComponent
+  },
+  {
+    path: 'user/register',
+    component: RegisterComponent
   }
 ];
 
