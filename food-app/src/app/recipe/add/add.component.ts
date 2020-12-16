f3bde8e8aa42bd3ee7c3f05b0a0d2fe350e63ac1
Inik_21 <n.dzhunov@yahoo.com>
2020-12-16 16:22:09 +0200

    Add add recipe template

--- food-app/src/app/recipe/add/add.component.ts
+++ food-app/src/app/recipe/add/add.component.ts
@@ -12,4 +12,8 @@ export class AddComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  addRecipeHandler(data) {
+    
+  }
+
 }
--- food-app/src/app/recipe/recipe.module.ts
+++ food-app/src/app/recipe/recipe.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddComponent } from './add/add.component';
+import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [AddComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     AddComponent
