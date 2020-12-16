df41443ab703074d80511a5d95fb8ac19555567c
Inik_21 <n.dzhunov@yahoo.com>
2020-12-16 10:41:24 +0200

    Add logout method in usersService

--- food-app/src/app/user/users-service.service.ts
+++ food-app/src/app/user/users-service.service.ts
@@ -27,4 +27,8 @@ export class UsersServiceService {
     return this.http.post(this.apiUrl + '/users/register',data);
   }
 
+  logout(): Observable<any> {
+    return this.http.get(this.apiUrl + '/users/logout');
+  }
+
 }
